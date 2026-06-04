d0b59e1c5c3cc63f327abe1635612eccd772192e
t-m-salvador <tiagosallionfire@gmail.com>
2026-06-04 21:43:19 +0100

    feat: icon + splash

--- papelada/capacitor.config.ts
+++ papelada/capacitor.config.ts
@@ -1,9 +1,17 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'papelada',
-  webDir: 'www'
+  appId: 'com.papelada.app',
+  appName: 'Papelada',
+  webDir: 'www',
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 3000, // Tempo que o splash fica visível (3 segundos)
+      launchAutoHide: true,     // Esconde automaticamente após o tempo acima
+      backgroundColor: "#ffffff", // Cor de fundo se a imagem não esticar tudo
+      androidScaleType: "CENTER_CROP"
+    }
+  }
 };
 
 export default config;
